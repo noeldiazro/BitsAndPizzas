f3da5261d86bcaf83441c5170f7d0f132ba3311a
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-05-15 16:29:21 +0200

    Share action

--- app/src/main/java/es/montanus/bitsandpizzas/MainActivity.java
+++ app/src/main/java/es/montanus/bitsandpizzas/MainActivity.java
@@ -1,14 +1,18 @@
 package es.montanus.bitsandpizzas;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String SHARE_TEXT = "Want to join me for pizza?";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem shareMenuItem = menu.findItem(R.id.action_share);
+        final ShareActionProvider shareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(shareMenuItem);
+        shareActionProvider.setShareIntent(getIntent(SHARE_TEXT));
+
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private Intent getIntent(String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        return intent;
     }
 
     @Override

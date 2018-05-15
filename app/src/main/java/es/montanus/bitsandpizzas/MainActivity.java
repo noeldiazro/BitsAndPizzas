3cf9d5d3cd399bab6e2beb1c2f7c7c7eb3588332
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-05-15 11:05:57 +0200

    Action

--- app/src/main/java/es/montanus/bitsandpizzas/MainActivity.java
+++ app/src/main/java/es/montanus/bitsandpizzas/MainActivity.java
@@ -1,8 +1,11 @@
 package es.montanus.bitsandpizzas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_create_order) {
+            startActivity(new Intent(this, OrderActivity.class));
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
---
+++ app/src/main/java/es/montanus/bitsandpizzas/OrderActivity.java
@@ -0,0 +1,15 @@
+package es.montanus.bitsandpizzas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+    }
+}

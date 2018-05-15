d1a0257e025249a5b65674091e67045a594e1fd2
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-05-15 11:28:42 +0200

    Up button

--- app/src/main/java/es/montanus/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/es/montanus/bitsandpizzas/OrderActivity.java
@@ -1,5 +1,6 @@
 package es.montanus.bitsandpizzas;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -11,5 +12,11 @@ public class OrderActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        enableUpButton();
+    }
+
+    private void enableUpButton() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }

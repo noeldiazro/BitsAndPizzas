eb0e9c5a2067c18ca413782b56a02303636ac1b2
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-05-20 21:12:51 +0200

    FAB

--- app/src/main/java/es/montanus/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/es/montanus/bitsandpizzas/OrderActivity.java
@@ -1,9 +1,10 @@
 package es.montanus.bitsandpizzas;
 
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -17,5 +18,9 @@ public class OrderActivity extends AppCompatActivity {
 
     private void enableUpButton() {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onClickDone(View view) {
+        Toast.makeText(this, "Done!", Toast.LENGTH_SHORT).show();
     }
 }

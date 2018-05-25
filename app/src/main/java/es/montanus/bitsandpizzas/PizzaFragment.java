df703f7a530f16066b8b2c1272c3f47ae70b34f4
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-05-25 17:56:32 +0200

    Recycler view

--- app/src/main/java/es/montanus/bitsandpizzas/PizzaFragment.java
+++ app/src/main/java/es/montanus/bitsandpizzas/PizzaFragment.java
@@ -1,10 +1,28 @@
 package es.montanus.bitsandpizzas;
 
-import android.support.annotation.ArrayRes;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class PizzaFragment extends CategoryFragment {
+public class PizzaFragment extends Fragment {
+    @Nullable
     @Override
-    protected @ArrayRes int getStringArrayId() {
-        return R.array.pizzas;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        RecyclerView pizzaRecycler = (RecyclerView) inflater.inflate(
+                R.layout.fragment_pizza, container, false);
+
+        pizzaRecycler.setAdapter(new CaptionedImagesAdapter(Pizza.pizzas));
+        pizzaRecycler.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        
+        return pizzaRecycler;
     }
 }
